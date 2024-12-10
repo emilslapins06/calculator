42be8dc2130083063706459b07f2eff097080eed
emilslapins06 <145561786+emilslapins06@users.noreply.github.com>
2024-12-10 14:33:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
-function addToDisplay(value) {
+function addToDisplay(value) 
+{
     document.getElementById('display').value += value;
 }
 
-function clearDisplay() {
+function clearDisplay() 
+{
     document.getElementById('display').value = '';
 }
 
-function calculate() {
-    try {
+function calculate() 
+{
+    try 
+    {
         const display = document.getElementById('display');
         const result = eval(display.value);
         display.value = result;
         addToHistory(display.value);
-    } catch (error) {
+    } 
+    catch (error) 
+    {
         document.getElementById('display').value = 'Error';
     }
 }
 
-function addToHistory(entry) {
+function addToHistory(entry) 
+{
     const historyList = document.getElementById('history');
     const listItem = document.createElement('li');
     listItem.textContent = entry;
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'clear';
+    removeButton.addEventListener('click', () => {historyList.removeChild(listItem)});
+    listItem.appendChild(removeButton);
     historyList.appendChild(listItem);
 }
 
-function clearHistory() {
+function clearHistory() 
+{
     const historyList = document.getElementById('history');
     historyList.innerHTML = '';
 }
+
